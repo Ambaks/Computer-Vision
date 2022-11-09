230795621de970ebdcfd73cc3f49c844c64b3358
Ambaks <78621642+Ambaks@users.noreply.github.com>
2022-11-09 10:12:55 -0600

    Created squat object
    Created a Squat class

--- squat_module.py
+++ squat_module.py
@@ -1,60 +1,75 @@
 import datetime
-from datetime import timedelta
 import cv2
 import numpy as np
 import PoseEstimationModule as pm
 import matplotlib.pyplot as plt
 
-def squat(filename):
 
-    plt.style.use('fivethirtyeight')
+class Squat():
+    def __init__(self, tlist = [], ylist = [], dir = 0, count = -.5, detector = pm.poseDetector()):
+        self.ylist = ylist
+        self.tlist = tlist
+        self.dir = dir
+        self.count = count
+        self.detector = detector
 
-    cap = cv2.VideoCapture(filename)
-    detector = pm.poseDetector()
-    count = -.5
-    dir = 0
-    tlist = []
-    ylist = []
+    def doSquat(self):
+
+        plt.style.use('fivethirtyeight')
+
+        cap = cv2.VideoCapture('squats.mp4')
+        # detector = pm.poseDetector()
+        # count = -.5
+        # dir = 0
+        # tlist = []
+        # ylist = []
 
 
-    while True:
-        success, img = cap.read()
-        if success:
-            img = detector.findPose(img, draw=False)
-            lmlist = detector.findPosition(img, draw=False)
+        while True:
+            success, img = cap.read()
+            if success:
+                img = self.detector.findPose(img, draw=False)
+                lmlist = self.detector.findPosition(img, draw=False)
 
 
-            if len(lmlist) != 0:
-                angle = detector.find_angle(img, 24, 26, 28)
-                per = np.interp(angle,(55, 180),(0, 100))
+                if len(lmlist) != 0:
+                    self.angle = self.detector.find_angle(img, 24, 26, 28)
+                    self.per = np.interp(self.angle,(55, 180),(0, 100))
 
-                # Check for squats
-                if per >= 95:
-                    if dir == 0:
-                        count += 0.5
-                        dir = 1
+                    # Check for squats
+                    if self.per >= 95:
+                        if self.dir == 0:
+                            self.count += 0.5
+                            self.dir = 1
 
-                if per <= 7:
-                    if dir == 1:
-                        count += 0.5
-                        dir = 0
+                    if self.per <= 7:
+                        if self.dir == 1:
+                            self.count += 0.5
+                            self.dir = 0
 
-                ylist.append(int(per))
-                tlist.append(datetime.datetime.now())
+                    self.ylist.append(int(self.per))
+                    self.tlist.append(datetime.datetime.now())
 
-            cv2.putText(img, f'Reps: {str(int(count))}', (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
-        else:
-            plt.plot_date(tlist, ylist)
-            plt.title('Squat Range Of Motion / time')
-            plt.gcf().autofmt_xdate()
-            plt.show()
-            rep_time = max(tlist) - min(tlist)
-            print(f'Number of reps: {count} reps')
-            print(f'Set length: {rep_time.seconds} seconds')
-            print(f'Average rep length: {round(rep_time.seconds/count, 2)} seconds')
-            break
+                cv2.putText(img, f'Reps: {str(int(self.count))}', (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
+            else:
+                plt.plot_date(self.tlist, self.ylist)
+                plt.title('Squat Range Of Motion / time')
+                plt.gcf().autofmt_xdate()
+                plt.show()
+                self.rep_time = max(self.tlist) - min(self.tlist)
+                print(f'Number of reps: {self.count} reps')
+                print(f'Set length: {self.rep_time.seconds} seconds')
+                print(f'Average rep length: {round(self.rep_time.seconds/self.count, 2)} seconds')
+                break
 
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-    return count, rep_time
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)
+        return self.count, self.rep_time
 
+
+def main():
+    perform = Squat()
+    perform.doSquat()
+
+if __name__ == '__main__':
+    main()
